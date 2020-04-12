b2bee7eed55a212933592cf9b67c0c6c9e2c3478
Matthew Bellafaire <matthew.bellafaire@gmail.com>
2020-04-12 13:30:52 -0400

    Removed IIC communication code

--- Space Time Code/Sources/main.c
+++ Space Time Code/Sources/main.c
@@ -2,24 +2,7 @@
 #include "derivative.h"      /* derivative-specific definitions */
 #include "declarations.h"
 
- static const char numbertable[] = {
-  0x3F, /* 0 */
-  0x06, /* 1 */
-  0x5B, /* 2 */
-  0x4F, /* 3 */
-  0x66, /* 4 */
-  0x6D, /* 5 */
-  0x7D, /* 6 */
-  0x07, /* 7 */
-  0x7F, /* 8 */
-  0x6F, /* 9 */
-  0x77, /* a */
-  0x7C, /* b */
-  0x39, /* C */
-  0x5E, /* d */
-  0x79, /* E */
-  0x71, /* F */
-};
+
 
 unsigned int  displaybuffer[8]; 
 
@@ -40,8 +23,7 @@ char i = 0;
 //initalization function (probably going to spin off other functions for UART and IIC)
 //for now just handles buttons
 void init(){
-  initButtons();
-  initI2C();
+
 }
 
 
@@ -55,30 +37,10 @@ void main(void) {
 	EnableInterrupts;
   
   for(;;) {
-   sendI2CDisplayCommand(dispAddr, 0x00,   0x3F);	
+  // sendI2CDisplayCommand(dispAddr, 0x00,   0x3F);	
   //button test code, if a button is pressed this code just lights up that button
   //note how mapping in declarations.h allows us to read the buttons   
-    if(!SW1){
-      lightButton(1, 255);   
-    } else{
-      lightButton(1, 0); 
-    }
-    if(!SW2){
-      lightButton(2, 255);   
-    } else{
-      lightButton(2, 0); 
-    }
-    
-    if(!SW3){
-      lightButton(3, 255);   
-    } else{
-      lightButton(3, 0); 
-    }
-    if(!SW4){
-      lightButton(4, 255);   
-    } else{
-      lightButton(4, 0); 
-    }
+  PORTB = 0xFF ^ PTH;
 
   }
 }
